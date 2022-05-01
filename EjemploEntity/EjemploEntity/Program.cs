c4f5e6751ff83d455f2519c63d135c094d69e286
Jas Menjivar <mm0588032021@unab.edu.sv>
2022-04-30 20:02:51 -0600

    BAR

--- EjemploEntity/EjemploEntity/Program.cs
+++ EjemploEntity/EjemploEntity/Program.cs
@@ -12,7 +12,7 @@ namespace EjemploEntity
         {
             using (BarDbEntities db= new BarDbEntities())
             {
-                /*Producto oproducto=new Producto();
+                Producto oproducto=new Producto();
                 Console.WriteLine("Nombre");
                 oproducto.nomProd=Console.ReadLine();
                 Console.WriteLine("Descripcion");
@@ -22,20 +22,32 @@ namespace EjemploEntity
                 Console.WriteLine("Cantidad"); 
                 oproducto.cantidad = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine("Estado");
-                oproducto.estado = Convert.ToInt32(Console.ReadLine()); ;
+                oproducto.estado = Convert.ToInt32(Console.ReadLine()); 
+
+
+                oproducto.nomProd = "trenzuda";
+                oproducto.descripcion = "Medio litro";
+                oproducto.precio = 20;
+                oproducto.cantidad = 50;
+                oproducto.estado = 1;
+                
 
                 db.Producto.Add(oproducto);
                 db.SaveChanges();
+
+                /*
+
+
                 //Actalizar/
                 Producto oproducto = db.Producto.Find(1);
                 oproducto.nomProd = "chamacoss";
                 db.Entry(oproducto).State = System.Data.Entity.EntityState.Modified;
-                db.SaveChanges();*/
+                db.SaveChanges();
 
                 //borrar//
                 Producto oproducto = db.Producto.Find();
                 db.Producto.Remove(oproducto);
-                db.SaveChanges();
+                db.SaveChanges();*/
 
                 var list = db.Producto;
                 foreach(var oproductos in list)
